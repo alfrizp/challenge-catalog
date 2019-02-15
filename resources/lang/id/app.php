2218dad82395a7c4972967db13613e34a8f86bb9
alip <alifrizkipambudi@gmail.com>
2019-02-15 22:46:52 +0700

    create dictionary resource

---
+++ resources/lang/id/app.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    // Labels
+    'app_name' => env('APP_NAME'),
+    'action' => 'Pilihan',
+    'home' => 'Halaman Depan',
+    'supplier' => 'Supplier',
+    'product' => 'Produk',
+
+    // Actions
+    'next' => 'Selanjutnya',
+    'previous' => 'Sebelumnya',
+    'edit' => 'Edit',
+    'delete' => 'Hapus',
+
+    // Attributes
+    'name' => 'Nama',
+    'email' => 'Email',
+    'city' => 'Kota',
+    'age' => 'Umur',
+
+];
---
+++ resources/lang/id/supplier.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    // Labels
+    'title' => 'List Semua Supplier',
+    'title_h2' => 'Data Supplier',
+    'add_supplier' => 'Tambah Supplier',
+    'edit_supplier' => 'Edit Supplier',
+    'delete_supplier' => 'Edit Supplier',
+    'name_placeholder' => 'Nama Supplier',
+    'email_placeholder' => 'Email Supplier',
+    'city_label' => 'Kota Asal',
+    'city_placeholder' => 'Pilih kota',
+    'birth_year_label' => 'Tahun Kelahiran',
+    'birth_year_placeholder' => 'Pilih tahun kelahiran',
+
+    // Actions
+    'save' => 'Simpan',
+    'cancel' => 'Batal',
+    'edit' => 'Edit',
+    'delete_confirm' => 'Anda yakin ingin menghapus?',
+    'delete_confirm_btn' => 'Ya!',
+    'created' => 'Berhasil menambahkan data Supplier baru',
+    'updated' => 'Berhasil mengedit data Supplier',
+    'deleted' => 'Berhasil menghapus data Supplier',
+
+    // Attributes
+    'name' => 'Nama',
+    'email' => 'Email',
+    'city' => 'Kota',
+    'age' => 'Umur',
+
+];
